d4c59eedcf1fb1b32d8e1c476e589a2d37571927
HW Jang <royjang12@gmail.com>
2025-05-25 23:10:50 +0900

    Implement and integrate ROI (Region of Interest) management and UI interaction

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from utils import get_resource_path
 from ui import AstromapperMainWindow, ImageWidget, LogWidget
 from PySide6 import QtWidgets, QtGui
 from PySide6.QtCore import QResource
+import logging
+import traceback
 
 def setup_application():
     """애플리케이션의 기본 설정을 초기화합니다."""
@@ -51,6 +53,7 @@ def connect_signals(image_widget: ImageWidget, log_widget: LogWidget):
 #     # 로그 위젯 -> 이미지 위젯 시그널
     log_widget.subImageChecked.connect(image_widget.on_checkbox_state_changed)
     log_widget.subImageSliderChanged.connect(image_widget.on_slider_value_changed)
+    log_widget.updateImgSignal.connect(image_widget.update_img)
 #     log_widget.image_upload_requested.connect(image_widget.open_image)
 #     log_widget.sub_image_check.connect(image_widget.on_checkbox_state_changed)
 #     log_widget.slider_changed.connect(image_widge t.on_slider_value_changed)
@@ -59,36 +62,54 @@ def connect_signals(image_widget: ImageWidget, log_widget: LogWidget):
 #     log_widget.extract_signal.connect(image_widget.extract_cosmosort_protocol)
 
 # TODO
-# Points들 디자인
-# 1. shift통해서 로그에 저장하기
 # 2. ROIs들 이미지 형태로 만들어서 효율적으로 관리하기
-# 3. ROI 클릭하면 해당 부분으로 이동하기
 # 4. 레퍼런스 기능 추가하기
+
+
 # 5. 영역선정 시에 이미지에 글씨 추가하기 - 글씨 크기를 조절할 수 있어야 함.
-# 6. svs 이미지 대응
+# 6. svs 이미지 대응 -> svs로드해서 이미지로 받으면 추후 프로세스 동일!
+# 6. ROIs들 이미지 출력하는 기능 추가
+# TOOL에 네모
+
+# 박스 선택하면 드래그로 이동 되도록 및 사이즈 조절 되게
+# lOG에 HOVER기능 추가
+# 3. ROI 클릭하면 해당 부분으로 이동하기
+
+# 세이브 기능 구현
+# 로그는 프로젝트파일에 저장하도록
+# ROI들 드래거블하게 및 오른쪽 클릭통한 기능 추가
 # 7. 라지 이미지 대응
 # 8. cam 기능 추가
 # 9. 이미지정보 받아오기 - 예외처리 추가
+# 11. 셀소터 및 이미지 분석 기능 추가.
+# 레퍼런스 등록되면 실제 길이와 변환 가능하도록.
+# 웰 이어서 기능 추가
+# Control Z 기능 추가 (10번까지)
+# 웰 이전꺼보다 다음으로 지금은 길이 기반
 
 def main():
     """애플리케이션의 메인 진입점입니다."""
     # # 설정 로드
+    # logging.basicConfig(
+    #     filename='log.txt',
+    #     level=logging.INFO,
+    #     format='[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s] %(message)s'
+    # )
     # config = Config()
-    
-    # 애플리케이션 설정
+    # # 애플리케이션 설정
     app = setup_application()
     
     # 메인 윈도우 생성
     window = create_main_window()
     
-    # 윈도우 아이콘 설정
+    # # 윈도우 아이콘 설정
     icon_path = get_resource_path(os.path.join("res", "images", "Astromapper.ico"))
     window.setWindowIcon(QtGui.QIcon(icon_path))
     
-    # 윈도우 표시
+    # # 윈도우 표시
     window.show()
     
-    # 애플리케이션 실행
+    # # 애플리케이션 실행
     sys.exit(app.exec())
 
 if __name__ == "__main__":
